f8c542b5accb12d9a7df8fd7383c048f0d889b40
YANGJIAKUN <200000725@163.com>
2020-09-14 05:14:40 +0800

    删除console

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-console.log(
-	`======================================================>
-==	You've turned on F12
-======================================================>`
-)
+
 Vue.config.productionTip = false
 
 new Vue({
