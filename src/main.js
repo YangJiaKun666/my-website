d9b821a409a6dec82d9251cecb7195a079baaae3
YANGJIAKUN <200000725@163.com>
2020-09-18 20:57:05 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+const userAgentInfo = navigator.userAgent.toLocaleLowerCase()
+if (userAgentInfo.indexOf('mobile')) {
+	window.location.href = 'http://yjk-dev.com:8302/'
+}
+
 new Vue({
 	router,
 	store,
